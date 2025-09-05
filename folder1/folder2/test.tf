789b44298702b436135ac45baa404614e1afa886
pakhunte <pakhunte2@gmail.com>
2025-09-05 13:26:59 -0700

    Create test.tf

---
+++ folder1/folder2/test.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Generate a random, memorable pet name with a prefix.
+resource "random_pet" "server_name" {
+  prefix = "app-server"
+  length = 2
+}
+
+# Generate a random UUID.
+resource "random_uuid" "my_uuid" {}
+
+# Use the randomly generated values in an output.
+output "server_pet_name" {
+  value = random_pet.server_name.id
+}
+
+output "generated_uuid" {
+  value = random_uuid.my_uuid.result
+}
